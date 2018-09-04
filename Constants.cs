7eb72126cdafd08b7bedce6b3feeb37b75f97ce8
Mike Smeltzer <mike@mikesmeltzer.com>
2018-09-04 09:42:48 -0300

    Initial commit of Connector extension sample module.

---
+++ Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MikeSmeltzer.Connectors.ConnectorExample
+{
+    internal class Constants
+    {
+
+        public const string LocalResourceFile = "~/DesktopModules/MikeSmeltzer/ConnectorExample/App_LocalResources/SharedResources.resx";
+
+    }
+}
